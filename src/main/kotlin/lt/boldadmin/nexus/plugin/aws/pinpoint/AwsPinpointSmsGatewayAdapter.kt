8bce81517a5c7ae4ee18105e47ccfd76c4e5156f
Okaminu <aurimasdgt@gmail.com>
2018-10-12 22:42:50 +0300

    BA-442: prototype for nexus integration to sms routing

--- src/main/kotlin/lt/boldadmin/nexus/plugin/aws/pinpoint/AwsPinpointSmsGatewayAdapter.kt
+++ src/main/kotlin/lt/boldadmin/nexus/plugin/aws/pinpoint/AwsPinpointSmsGatewayAdapter.kt
@@ -6,25 +6,33 @@ import com.amazonaws.services.pinpoint.model.*
 import lt.boldadmin.nexus.api.SmsGateway
 
 class AwsPinpointSmsGatewayAdapter: SmsGateway {
-    override fun send(phoneNumber: String, message: String) {
+    override fun send(fromPhoneNumber: String, toPhoneNumber: String, message: String) {
         AmazonPinpointClientBuilder.standard().withRegion(Regions.US_EAST_1).build().sendMessages(
             SendMessagesRequest().withMessageRequest(
-                MessageRequest().withAddresses(
-                    mapOf(
-                        Pair(
-                            phoneNumber,
-                            AddressConfiguration().withChannelType(ChannelType.SMS)
-                        )
-                    )
-                ).withMessageConfiguration(
-                    DirectMessageConfiguration().withSMSMessage(
-                        SMSMessage().withBody(message)
-                            .withMessageType(MessageType.TRANSACTIONAL).withOriginationNumber(
-                                ""
-                            )
-                    )
-                )
+                createMessageRequest(toPhoneNumber, message, fromPhoneNumber)
             ).withApplicationId("fa31dc0f27c74def905f19179398b22e")
+        )
+    }
+
+    private fun createMessageRequest(
+        toPhoneNumber: String,
+        message: String,
+        fromPhoneNumber: String
+    ): MessageRequest? {
+        return MessageRequest().withAddresses(
+            mapOf(
+                Pair(
+                    toPhoneNumber,
+                    AddressConfiguration().withChannelType(ChannelType.SMS)
+                )
+            )
+        ).withMessageConfiguration(
+            DirectMessageConfiguration().withSMSMessage(
+                SMSMessage().withBody(message)
+                    .withMessageType(MessageType.TRANSACTIONAL).withOriginationNumber(
+                        fromPhoneNumber
+                    )
+            )
         )
     }
 }
